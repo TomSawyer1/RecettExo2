6421875ddf6c84d5cbc1cedf487dd9f0fb9a4660
thomas.andrade <thomas.andrade@europcar.com>
2024-03-06 09:28:40 +0100

    Fin

---
+++ app/Storage/Contracts/RecipeStorageInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Storage\Contracts;
+
+use App\Models\Recipe;
+
+interface RecipeStorageInterface
+{
+    public function all();
+    public function find($id);
+    public function store(Recipe $recipe);
+    public function update($id, Recipe $recipe);
+    public function delete($id);
+}
